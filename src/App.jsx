ffbde22332490cf6bc29ad40cf77ffdcc67e5184
Ailuu17 <aile17wc@gmail.com>
2025-09-25 14:25:58 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ContadorComponent from "./Componentes/contadorComponent";
+import PersonaComponent from "./Componentes/personaComponent";
+
+function App() {
+
+  return (
+    <>
+      <ContadorComponent />
+      <PersonaComponent />
+    </>
+  )
+}
+
+export default App;
